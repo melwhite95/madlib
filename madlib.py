384757003268f57f84840e480b04cd2a6ecb681b
melwhite95 <melwhite3895@gmail.com>
2018-10-31 17:20:27 -0700

    first commit

---
+++ madlib.py
@@ -0,0 +1,35 @@
+madlibs = story()
+
+def madLibs1():
+#adjetive
+adj = input('enter a descreptive word')
+#animal
+animal = input('insert animal of your choosing')
+#noun name
+noun_name = input('Enter a name')
+#occupation
+occupation = input('enter a job or career')
+#pronoun
+pronuoun = input('insert appropiate pronoun')
+
+result = "The {} {} walked down to the church. Every Sunday {} shoots {} shot with the {} daughter."
+ print(result.format(adj,animal,noun_name,pronoun,occupation))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+The green dog walked down to the church. Every Sunday Daniel shoots his shot with the pastors daughter.
