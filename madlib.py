4106a417555eb00757e35662d4a9296ebd47fdb7
melwhite95 <melwhite3895@gmail.com>
2018-12-06 13:04:06 -0800

    aybe done

--- madlib.py
+++ madlib.py
@@ -1,18 +1,20 @@
 ##madlibs = story()
 
-def mad_Libs1():
-    print("welcome to the party")
+
+#def mad_Lib():
+print("welcome to the party")
 
 #adjetive
-    a_adjetive = input('enter a descreptive word')
+adjetive = input('enter a descreptive word')
+    #print("this is the a_adjetive variable",a_adjetive)
 #animal
-    a_animal = input('insert animal of your choosing')
+animal = input('insert animal of your choosing')
 #noun name
-    a_proper_noun = input('Enter a name')
+proper_noun = input('Enter a name')
 #occupation
-    a_occupation = input('enter a job or career')
+occupation = input('enter a job or career')
 #pronoun
-    a_pronuoun = input('insert appropiate pronoun')
+pronoun = input('insert appropiate pronoun')
 
-result = "The {} {} walked down to the church. Every Sunday {} shot with the + {} son."
-#print(result.format(adjetive,animal,noun_name,pronoun,occupation))
+result = "The {} {} walked down to the church. Every Sunday {} shoots {} shot with the  {} son."
+print(result.format(adjetive,animal,proper_noun,pronoun,occupation))
